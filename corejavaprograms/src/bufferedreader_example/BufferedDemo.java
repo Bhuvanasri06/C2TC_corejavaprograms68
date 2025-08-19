3e6ca9aa51cb06c936fac66a457a8fa89162519f
admin <admin@MVLEO-DESKTOP>
2025-08-19 14:45:13 +0530

    singleinheritance_example

---
+++ corejavaprograms/src/bufferedreader_example/BufferedDemo.java
@@ -0,0 +1,18 @@
+package bufferedreader_example;
+import java.io.*;
+
+public class BufferedDemo {
+
+	public static void main(String[] args) throws IOException
+	{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in))
+System.out.println("Enter the name:");
+String name=br.readLine();
+System.out.println("Enter the age:");
+int age=Integer.parseInt(br.readLine());
+System.out.printLn("Enter the salary:");
+double salary=
+
+	}
+
+}
