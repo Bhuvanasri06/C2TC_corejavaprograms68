66b702a375a0b83ae95dfc8d01d78e9645a3d4fb
admin <admin@MVLEO-DESKTOP>
2025-08-20 14:12:08 +0530

    this project contains all core java programs

---
+++ corejavaprograms/src/com/tnsif/abstract_example/TechTrainer.java
@@ -0,0 +1,13 @@
+package com.tnsif.abstract_example;
+
+public class TechTrainer extends Trainer {
+	public void session()
+	{
+		System.out.println("Core java session is going on");
+	}
+	public void checks()
+	{
+		System.out.println("Assignments give from Inheritance");
+	}
+	
+}
